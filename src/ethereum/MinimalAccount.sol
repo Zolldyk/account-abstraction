7a1fdc07ab69b1eea4aa1890d6d380273d805c98
Zolldyck <buadesoye@gmail.com>
2025-04-21 00:14:33 +0100

    New scripts and test

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -1,30 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity 0.8.24;
 
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
-import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {SIG_VALIDATION_FAILED, SIG_VALIDATION_FAILED} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
 import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
-abstract contract MinimalAccount is IAccount, Ownable {
-    /*/////////////////////////////////////////////////////////////////
-    //                         ERRORS                               //
-    ///////////////////////////////////////////////////////////////*/
+contract MinimalAccount is IAccount, Ownable {
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
     error MinimalAccount__NotFromEntryPoint();
     error MinimalAccount__NotFromEntryPointOrOwner();
-    error MinimalAccount__CallFailed(bytes result);
+    error MinimalAccount__CallFailed(bytes);
 
-    /*/////////////////////////////////////////////////////////////////
-    //                   STATE VARIABLES                            //
-    ///////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
     IEntryPoint private immutable i_entryPoint;
 
-    /*/////////////////////////////////////////////////////////////////
-    //                       MODIFIERS                              //
-    ///////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                               MODIFIERS
+    //////////////////////////////////////////////////////////////*/
     modifier requireFromEntryPoint() {
         if (msg.sender != address(i_entryPoint)) {
             revert MinimalAccount__NotFromEntryPoint();
@@ -39,19 +39,19 @@ abstract contract MinimalAccount is IAccount, Ownable {
         _;
     }
 
-    /*/////////////////////////////////////////////////////////////////
-    //                      FUNCTIONS                               //
-    ///////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
     constructor(address entryPoint) Ownable(msg.sender) {
         i_entryPoint = IEntryPoint(entryPoint);
     }
 
     receive() external payable {}
 
-    /*/////////////////////////////////////////////////////////////////
-    //                   EXTERNAL FUNCTION                          //
-    ///////////////////////////////////////////////////////////////*/
-    function execute(address dest, uint256 value, bytes calldata functionData) external requireFromEntryPoint {
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function execute(address dest, uint256 value, bytes calldata functionData) external requireFromEntryPointOrOwner {
         (bool success, bytes memory result) = dest.call{value: value}(functionData);
         if (!success) {
             revert MinimalAccount__CallFailed(result);
@@ -59,30 +59,31 @@ abstract contract MinimalAccount is IAccount, Ownable {
     }
 
     // A signature is valid, if it's the MinimalAccount owner
-    function validatedUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
         requireFromEntryPoint
         returns (uint256 validationData)
     {
         validationData = _validateSignature(userOp, userOpHash);
+        // _validateNonce()
         _payPrefund(missingAccountFunds);
     }
 
-    /*/////////////////////////////////////////////////////////////////
-    //                   INTERNAL FUNCTION                          //
-    ///////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
     // EIP-191 version of the signed hash
     function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
         internal
         view
         returns (uint256 validationData)
     {
-        bytes32 ethSignatureMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
-        address signer = ECDSA.recover(ethSignatureMessageHash, userOp.signature);
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
         if (signer != owner()) {
             return SIG_VALIDATION_FAILED;
         }
-        return SIG_VALIDATION_FAILED;
+        return SIG_VALIDATION_SUCCESS;
     }
 
     function _payPrefund(uint256 missingAccountFunds) internal {
@@ -92,9 +93,9 @@ abstract contract MinimalAccount is IAccount, Ownable {
         }
     }
 
-    /*/////////////////////////////////////////////////////////////////
-    //                          GETTERS                             //
-    ///////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
     function getEntryPoint() external view returns (address) {
         return address(i_entryPoint);
     }
